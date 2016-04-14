d6efca8906af3e19e0623f718ff786c16c878218
enrique.munguia <enrique.munguia@globant.com>
2016-04-14 17:39:57 -0500

    Add side color with inline style

--- source/Card.js
+++ source/Card.js
@@ -26,8 +26,19 @@ export default class Card extends Component {
       )
     }
 
+    let sideColor = {
+      position: 'absolute',
+      zIndex: -1,
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: 7,
+      backgroundColor: this.props.color
+    };
+
     return (
       <div className='card'>
+        <div style={ sideColor }/>
         <div className={this.state.showDetails ? 'card__title card__title--is-open' : 'card__title'}
             onClick={ this.toggleDetails.bind(this) }>
           { this.props.title }
