f06236ee7034e6eac16b777a0e237812d6a60b64
enrique.munguia <enrique.munguia@globant.com>
2016-04-14 17:37:46 -0500

    Use dangerouslySetInnerHTML

--- source/Card.js
+++ source/Card.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import CheckList from './CheckList'
+import marked from 'marked';
 
 export default class Card extends Component {
   constructor () {
@@ -8,13 +9,18 @@ export default class Card extends Component {
       showDetails: false
     }
   }
+
+  toggleDetails () {
+    this.setState({showDetails: !this.state.showDetails});
+  }
+
   render () {
     let cardDetails;
 
     if (this.state.showDetails) {
       cardDetails = (
         <div className='card__details'>
-          { this.props.description }
+          <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
           <CheckList cardId={ this.props.id } tasks={ this.props.tasks } />
         </div>
       )
@@ -22,7 +28,8 @@ export default class Card extends Component {
 
     return (
       <div className='card'>
-        <div className='card__title' onClick={ () => this.setState({showDetails: !this.state.showDetails}) }>
+        <div className={this.state.showDetails ? 'card__title card__title--is-open' : 'card__title'}
+            onClick={ this.toggleDetails.bind(this) }>
           { this.props.title }
         </div>
         { cardDetails }
