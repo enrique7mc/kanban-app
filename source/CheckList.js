9317488ba3ce74037760ae1caa3241117bc923fc
enrique.munguia <enrique.munguia@globant.com>
2016-04-15 02:06:15 -0500

    Add keys to CheckList and List

--- source/CheckList.js
+++ source/CheckList.js
@@ -4,8 +4,8 @@ export default class CheckList extends Component {
   render () {
     let tasks = this.props.tasks.map((task) => {
       return (
-        <li className='checklist__task'>
-          <input type='checkbox' defaultChecked={ task.done } />
+        <li key={ task.id } className='checklist__task'>
+          <input type='checkbox' checked={ task.done } />
           { task.name }
           <a href='#' className='checklist__task--remove' />
         </li>
@@ -15,6 +15,8 @@ export default class CheckList extends Component {
     return (
       <div className='checklist'>
         <ul>{ tasks }</ul>
+        <input type='text' className='checklist--add-tast'
+               placeholder='Type then hit Enter to add a task' />
       </div>
     );
   }
